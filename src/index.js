d28e3e46f965bb0d57fcb9cb033206b4812f6c71
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-06-18 16:09:36 +0300

    added start settings

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { GlobalStyle } from 'components/GlobalStyle';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+      <GlobalStyle />
+    </Provider>
   </React.StrictMode>
 );
