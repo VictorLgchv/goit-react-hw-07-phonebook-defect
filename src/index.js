969d04c2b8256509fea2008d482b00b42c5b33c8
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-06-18 16:24:43 +0300

    added redux files

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { App } from 'components/App';
 import './index.css';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
-      <GlobalStyle />
+        <GlobalStyle />
+        </PersistGate>
     </Provider>
   </React.StrictMode>
 );
